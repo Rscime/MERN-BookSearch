f54d2f663f9b9f3eeff15255fe3f9a3afadd6a7d
Rscime <Rachelscime@gmail.com>
2023-09-25 12:31:29 -0400

    did dearchbook

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -11,6 +11,8 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import {useMutation} from '@apollo/client';
+import {SAVE_BOOK} from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -71,19 +73,30 @@ const SearchBooks = () => {
       return false;
     }
 
-    try {
-      const response = await saveBook(bookToSave, token);
+  //   try {
+  //     const response = await saveBook(bookToSave, token);
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+  //     if (!response.ok) {
+  //       throw new Error('something went wrong!');
+  //     }
 
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+  //     // if book successfully saves to user's account, save book id to state
+  //     setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // };
+
+  try {
+    await saveBook({
+      variables: { newBook: {...bookToSave} }
+    });
+    
+    setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+  } catch (e) {
+    console.error(e);
+  }
+};
 
   return (
     <>
